fd91113eb6535a514f108d41d61c159b493b3763
yaofanghao <yaofanghao7@163.com>
2023-04-21 22:04:33 +0800

    2023.4.21

---
+++ APUE_note/1_2_stdio.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: yao fanghao
+ * @Date: 2023-04-21 21:11:11
+ * @LastEditTime: 2023-04-21 21:24:43
+ * @LastEditors: yao fanghao
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+
+    exit(0);
+}
